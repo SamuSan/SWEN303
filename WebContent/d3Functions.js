5479e30789fa1768e64ca596395637d4d7d52e74
SamuSan <sambminns@gmail.com>
2014-05-13 15:43:40 +1200

    erth

---
+++ WebContent/d3Functions.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+var year = 2013;
+var pieInnerRadius = 50;
+var pieRadius = 150;
+
+
+var width = 2000;
+var height = 800;
+
+// var color = d3.scale.category10();
+
+function stripSpaces(str, classType){
+			var cssClassName = str.replace(/\s+/g, '');
+			var cssClass = cssClassName.concat(classType);
+			return cssClass;
+	
+}
+
---
+++ WebContent/teamData.js
@@ -0,0 +1,13 @@
+var teamNames = {
+	"Adelaide ThunderBirds" : "adelaideThunderBirds",
+	"Melbourne Vixens": "melbourneVixens",
+	"Central Pulse" : "centralPulse",
+	"Queensland Firebirds":"queensLandFireBirds",
+	"Waikato Bay of Plenty Magic":"waikatoBPMagic",
+	"Southern Steel":"southernSteel",
+	"New South Wales Swifts":"nswSwifts",
+	"Northern Mystics":"northernMystics",
+	"West Coast Fever":"westCoastFever",
+	"Canterbury Tactix":"canterburyTactix"
+
+};
