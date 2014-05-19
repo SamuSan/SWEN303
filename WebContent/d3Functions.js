3f4e652a76dd5da8ea2b1db3ef86234ae682fffc
SamuSan <sambminns@gmail.com>
2014-05-19 21:54:12 +1200

    Looking reasonably sexy like your Mum

--- WebContent/d3Functions.js
+++ WebContent/d3Functions.js
@@ -16,7 +16,10 @@ var pieRadius = 200;
 //var height = 500;
 
 // var color = d3.scale.category10();
-
+function initial(str){
+ var strNew =   str.replace( /[^A-Z]/g, '' );
+    return strNew;
+}
 function stripSpaces(str, classType){
 			var cssClassName = str.replace(/\s+/g, '');
 			var cssClass = cssClassName.concat(classType);
